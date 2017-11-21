e0aab216e2c85146eb882ee8debbb1057674be1d
uumu <umu.kawakubo@gmail.com>
2017-11-21 13:45:08 +0000

    ListFiles completed

--- src/com/company/FileList.java
+++ src/com/company/FileList.java
@@ -1,72 +1,70 @@
 /*
 Program that takes a filepath as command-line argument and recursively prints the contents of the directories
 it encounters.
-Uses array-based implementation.
-
-1.Evaluates the file system. Passes original directory files to enqueue
-  ???????If it finds any directories ??????
-2.enqueue the contents, if there is a new directory create a new list within the one we are currently in??
-3.Print the contents of the queues?
-
+Uses queue implementation.
  */
 package com.company;
 
 import java.io.File;
-import java.io.IOException;
 
-public class FileList {
-    private String[] arr;
-    private String dirPath;
-    private int first;
-    private int last;
-    private int n;
+public class FileList<Item> {
+    private Queue<String> q;
+    private int depth;
+    private static final String TAB = "  ";
 
-    public FileList(String dirPath){
-        arr = new String[2];
-        dirPath = dirPath;
-        first = 0;
-        last = 0;
-        n = 0;
+    public FileList(String rootDir){
+        q = new Queue<>();
+        depth = 0;
+        evaluateDirectory(new File(rootDir));
     }
 
-    public void evaluateDirectory() throws IOException {
-        File f = new File(dirPath);
-        //check directory is actually valid, if +not, close the program.
-        if(!f.isDirectory()){
-            throw new IOException("Path does not exist.");
-        }else{
-            //generate queue for the filepath including queues within queues for new directories found
-            String[] fileArr = f.list();
-
-            for(String file : fileArr){
-                System.out.println(file);
-            }
-
-            //if there is a directory in the filepath, call the printer method on that,too.
+    public void add(String fileName){
+        //method that adds the correct amount of indentation to the current file/directory
+        //and enqueues it to the queue ready for printing.
+        String indent = "";
+        for(int i = 0; i < depth; i++){
+            indent+=TAB;
         }
-
-
-    }
-    public boolean isEmpty(){
-        return n == 0;
+        q.enqueue(indent + fileName);
     }
 
-    public void push(String item) {
-        //FIFO push to first in the queue.
+    public void evaluateDirectory(File dir){
+        //evaluates current directory, adds all directories and files to add() method   
+        //which enqueues the string name to queue.
+        //if directory is found, this method is recalled on that directory.
+        if(!dir.isDirectory()){
+            throw new IllegalArgumentException("Path does not exist.");
+        }
+        add(dir.getName() + " -\\");
+        depth++;
+        File[] fileArr = dir.listFiles();
+        for(File file : fileArr){
+            if(file.isDirectory()){
+                evaluateDirectory(file);
+            }else{
+                add(file.getName());
+            }
+        }
+        depth--;
     }
 
-    public String printTree(){
-    //prints the output of the recursively added queue file system.
+    public void print(){
+        //prints the output of the queue file system.
+        for(String s: q){
+            System.out.println(s);
+        }
     }
 
-    public static void main(String[] args) throws IOException {
-        FileList q = new FileList(args[0]);
-        try{
-            q.evaluateDirectory();
-        }catch(IOException e){
-            System.out.println("Path is invalid");}
+    public static void usage(){
+        //prints correct usage if there is an inout error.
+        System.out.println("Usage: java FileList <directory path>");
+    }
 
-        System.out.println(q.printTree());
+    public static void main(String[] args) {
+        if (args.length < 1){ usage(); }
+
+        FileList tree = new FileList(args[0]);
+        tree.print();
     }
 
 }
